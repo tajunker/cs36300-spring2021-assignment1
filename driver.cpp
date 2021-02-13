f262ea611f3e0e3aadc3534317c05e2807a76f95
Tercio A. Junker <tajunker@iu.edul>
2021-02-13 00:10:36 -0500

    driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,8 +1,38 @@
 #include "Array.h"
+#include <iostream>
 
-int main (int argc, char * argv [])
-{
-  // TODO Add code here to test your Array class.
-  
-  return 0;
+
+void print(Array array);
+
+
+int main (int argc, char * argv []) {
+ 	
+ 	Array testingArray(10, 'z');
+
+ 	print(testingArray);
+
+ 	return 0;
+
+
+}
+
+
+void print(Array array) {
+
+	std::cout << 'TESTING ARRAY\n\n';
+
+	std::cout << '[';
+
+	for (int iterator = 0; iterator < array.size(); iterator++) {
+
+		std::cout << array[iterator];
+
+		if (iterator < ( array.size() - 1 ) )
+
+			std::cout << ',';
+
+	}
+
+	std::cout << "] \n";
+
 }
